f4b72232b40b3742d40d3653e70c69bb4e3c6666
gk7734 <gamekiller7734@naver.com>
2023-08-25 21:56:18 +0900

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-[#F8F3ED] bg-no-repeat bg-cover ${inter.className}`}>
+      {children}
+      </body>
     </html>
   )
 }
